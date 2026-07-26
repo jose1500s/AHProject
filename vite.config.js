0f25defc0511f94ee979f7961a83e075005b5a9c
Jose Antonio <confi15826s@gmail.com>
2026-07-26 01:17:12 -0600

    setup client side with inertia

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue'
+import inertia from '@inertiajs/vite'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
+            refresh: true
         }),
         tailwindcss(),
+        vue(),
+        inertia()
     ],
     server: {
         watch: {
