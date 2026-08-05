5da442baef7dd443ca01835c4f33bed984926bcc
Jose Antonio <confi15826s@gmail.com>
2026-08-05 14:58:36 -0600

    update info badge and refresh button added

--- resources/js/Pages/Layout.vue
+++ resources/js/Pages/Layout.vue
@@ -2,11 +2,17 @@
 import { ref } from 'vue'
 import InputText from './Components/InputText.vue';
 import CustomSelect from './Components/CustomSelect.vue';
+import TimeAgoBadge from './Components/TimeAgoBadge.vue';
+import RefreshButton from './Components/RefreshButton.vue';
 
 const region = ref('US')
 const realm = ref('Illidan')
 
 const realms = ['Illidan', 'Stormrage', 'Tichondrius', "Mal'Ganis", 'Area 52', 'Sargeras', 'Bleeding Hollow', 'Frostmourne']
+
+function onRefresh() {
+    // llamada para refrescar datos
+}
 
 </script>
 
@@ -26,6 +32,10 @@ const realms = ['Illidan', 'Stormrage', 'Tichondrius', "Mal'Ganis", 'Area 52', '
                 <div class="flex gap-3">
                     <CustomSelect v-model="region" :options="['US', 'EU', 'KR', 'TW']" label="Region" />
                     <CustomSelect v-model="realm" :options="realms" label="Realm" />
+                </div>
+                <div class="flex items-center gap-3 mt-5 ml-4">
+                    <TimeAgoBadge label="1h ago" />
+                    <RefreshButton @click="onRefresh" />
                 </div>
             </div>
         </nav>
