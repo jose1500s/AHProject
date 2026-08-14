cb39bed1bff349dcbb52fd9153a2fa678c87674f
Jose Antonio <confi15826s@gmail.com>
2026-08-14 01:39:14 -0600

    item cards change

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -11,15 +11,13 @@ const props = defineProps({
 <template>
     <Layout :realms="realms">
         <div class="flex mt-5 items-center justify-center gap-5 w-[90vw] mx-auto">
-            <ItemCard 
-                name="Flor de Tranquilidad"
-                subtitle="Planta · Herboristeria"
-                :gold="118"
-                :silver="40"
-                :copper="20"
-                :listings="312"
-                :volume="1820" 
-            />
+            <div class="grid grid-cols-2 gap-3">
+                <ItemCard name="Storm Dust" quality="uncommon" :gold="95" :listings="2100" :volume="8908" />
+                <ItemCard name="Runed Adamantite Rod" quality="rare" :gold="74" :listings="74" :volume="320" />
+                <ItemCard name="Legguards of Spectral..." quality="epic" :gold="560" :listings="16" :volume="52" />
+                <ItemCard name="Shadowed Parchment..." quality="common" :gold="1" :silver="20" :listings="1200"
+                    :volume="5400" />
+            </div>
         </div>
     </Layout>
 </template>
